c8d707426e4d31861ae6d30833f881326541c217
kimu805 <kimukimuok805@gmail.com>
2025-04-08 15:37:33 +0900

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "アソシエーションのテスト" do
+    before do
+      @article = Article.create(title: "RSpecの基本", content: "RSpecを学びましょう。")
+
+      @comment_1 = Comment.create(content: "良い記事ですね！", article: @article)
+      @comment_2 = Comment.create(content: "分かりやすいです！", article: @article)
+    end
+
+    it "コメントを関連付けられる" do
+      expect(@article.comments).to include(@comment_1, @comment_2)
+    end
+
+    it "記事に属していることを確認する" do
+      expect(@comment_1.article).to eq(@article)
+    end
+  end
 end
