dca1e6680b7c3eb502d09d73599568180ce7fbbd
kimu805 <kimukimuok805@gmail.com>
2025-04-08 14:52:21 +0900

    Update article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "記事を正しく作成できる" do
+    article = Article.new(title: "RSpecの基本", content: "RSpecを学びましょう")
+    expect(article).to be_valid
+  end
 end
