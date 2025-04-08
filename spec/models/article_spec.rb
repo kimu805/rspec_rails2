b94db1e8ce0233c1cf3e1a79c3f22cc436d01c61
kimu805 <kimukimuok805@gmail.com>
2025-04-08 15:05:36 +0900

    Update article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,5 +9,17 @@ RSpec.describe Article, type: :model do
   end
 
   context "異常系" do
+    it "タイトルがなければ無効" do
+      article = Article.new(title: nil, content: "RSpecを学びましょう。楽しいですよ！")
+      expect(article).not_to be_valid
+    end
+    it "コンテンツがなければ無効" do
+      article = Article.new(title: "RSpecの基本", content: nil)
+      expect(article).not_to be_valid
+    end
+    it "コンテンツが10文字未満なら無効" do
+      article = Article.new(title: "RSpecの基本", content: "短い")
+      expect(article).not_to be_valid
+    end
   end
 end
