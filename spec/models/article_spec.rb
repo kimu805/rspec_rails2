090ffa3308081a2ab21ae0dc1f623daf9ff58720
kimu805 <kimukimuok805@gmail.com>
2025-04-08 15:54:43 +0900

    Update article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,34 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  subject { article.valid? }
   let(:title) { "RSpecの基本" }
   let(:content) { "RSpecを学びましょう。楽しいですよ！" }
-  let(:article) { Article.new(title: title, content: content) }
 
-  context "正常系" do
-    it "タイトルとコンテンツがあれば有効" do
-      expect(subject).to be true
+  describe "#comments_count" do
+    before do
+      @article = Article.create(title: title, content: content)
     end
-  end
 
-  context "異常系" do
-    context "タイトルがない場合" do
-      let(:title) { nil }
-      it "無効である" do
-      expect(subject).not_to be true
+    context "紐づくコメントがある場合" do
+      it "コメントの数を正しく返す" do
+        @article.comments.create(content: "hogehogehoge")
+        @article.comments.create(content: "fugafugafuga")
+
+        expect(@article.comments_count).to eq(2)
       end
     end
-    context "コンテンツがない場合" do
-      let(:content) { nil }
-      it "無効である" do
-        expect(subject).not_to be true
-      end
-    end
-    context "コンテンツが10文字未満の場合" do
-      let(:content) { "123456789" }
-      it "無効である" do
-        expect(subject).not_to be true
+
+    context "紐づくコメントがない場合" do
+      it "0を返す" do
+        expect(@article.comments_count).to eq(0)
       end
     end
   end
