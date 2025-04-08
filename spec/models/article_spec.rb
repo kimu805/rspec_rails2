53897968c449cc1424d377f301ec26e796f69593
kimu805 <kimukimuok805@gmail.com>
2025-04-08 14:59:56 +0900

    Update article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "記事を正しく作成できる" do
-    article = Article.new(title: "RSpecの基本", content: "RSpecを学びましょう")
-    expect(article).to be_valid
+  context "正常系" do
+    it "タイトルとコンテンツがあれば有効" do
+      article = Article.new(title: "RSpecの基本", content: "RSpecを学びましょう。楽しいですよ！")
+      expect(article).to be_valid
+    end
+  end
+
+  context "異常系" do
   end
 end
