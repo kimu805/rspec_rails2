8fe23e4e2223a302073ff34c6f10d255587468b1
kimu805 <kimukimuok805@gmail.com>
2025-04-09 17:51:39 +0900

    Update articles_spec.rb

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   describe "GET /articles" do
-    it "記事一覧ページの表示に成功する" do
+    before do
+      @article1 = Article.create(title: "最初の記事", content: "最初の記事の本文です。")
+      @article2 = Article.create(title: "2番目の記事", content: "2番目の記事の本文です。")
+    end
+
+    it "全ての記事を取得できる" do
       get "/articles"
       expect(response).to have_http_status(200)
+
+      json = JSON.parse(response.body)
+      expect(json.size).to eq(2)
+      expect(json.first["title"]).to eq("最初の記事")
+      expect(json.second["title"]).to eq("2番目の記事")
     end
   end
 end
