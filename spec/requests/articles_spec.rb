3ebdeab7485428668954991349d9bb9560650135
kimu805 <kimukimuok805@gmail.com>
2025-04-09 17:43:24 +0900

    Update articles_spec.rb

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/articles/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /articles" do
+    it "記事一覧ページの表示に成功する" do
+      get "/articles"
+      expect(response).to have_http_status(200)
     end
   end
-
 end
