3bf184545c078502bd4129ea11ee68d8e37f2b8b
kimu805 <kimukimuok805@gmail.com>
2025-04-09 17:54:11 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,19 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     render json: @articles
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
