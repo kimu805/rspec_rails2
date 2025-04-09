755dca8ce00c66b2a1c601bf1a22a965a0091013
kimu805 <kimukimuok805@gmail.com>
2025-04-09 18:32:46 +0900

    Update articles_spec.rb

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -76,6 +76,33 @@ RSpec.describe "Articles", type: :request do
         json = JSON.parse(response.body)
         expect(json["title"]).to eq("更新された記事")
         expect(json["content"]).to eq("これは更新された記事の内容です。")
+
+        article.reload
+        expect(article.title).to eq("更新された記事")
+        expect(article.content).to eq("これは更新された記事の内容です。")
+      end
+    end
+
+    context "無効なパラメータの場合" do
+      it "記事を更新できない" do
+        original_title = article.title
+        original_content = article.content
+
+        patch "/articles/#{article.id}", params: {
+          article: {
+            title: "",
+            content: ""
+          }
+        }
+
+        expect(response).to have_http_status(422)
+
+        json = JSON.parse(response.body)
+        expect(json["title"]).to include("blank")
+
+        article.reload
+        expect(article.title).to eq(original_title)
+        expect(article.content).to eq(original_content)
       end
     end
   end
