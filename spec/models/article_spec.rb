0bd288fabc564ae05ad4abac0b2ff94d1e393ee6
kimu805 <kimukimuok805@gmail.com>
2025-04-09 19:18:09 +0900

    Update article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,36 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   let(:title) { "RSpecの基本" }
   let(:content) { "RSpecを学びましょう。楽しいですよ！" }
+  subject { Article.new(title: title, content: content) }
+
+  describe "validation" do
+    context "正常系" do
+      it "タイトルとコンテンツがあれば有効" do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "異常系" do
+      context "タイトルがない場合" do
+        let(:title) { nil }
+        it "無効である" do
+          expect(subject).not_to be_valid
+        end
+      end
+      context "コンテンツがない場合" do
+        let(:content) { nil }
+        it "無効である" do
+          expect(subject).not_to be_valid
+        end
+      end
+      context "コンテンツが10文字より少ない場合" do
+        let(:content) { "123456789" }
+        it "無効である" do
+          expect(subject).not_to be_valid
+        end
+      end
+    end
+  end
 
   describe "#comments_count" do
     before do
