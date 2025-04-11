7c5fac8ee3ce7348ecb537a1379e64a2ed64b365
kimu805 <kimukimuok805@gmail.com>
2025-04-11 17:27:02 +0900

    Update todos_spec.rb

--- spec/system/todos_spec.rb
+++ spec/system/todos_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe "Todo管理", type: :system do
     expect(page).to have_content("これはテストTodoの説明です。")
     expect(page).to have_content("未完了")
   end
+
+  it "新規Todoを作成できる" do
+    visit new_todo_path
+
+    fill_in "タイトル", with: "RSpecを学ぶ"
+    fill_in "説明", with: "システムスペックについて理解を深める"
+
+    click_button "作成"
+
+    expect(current_path).to eq(todos_path)
+
+    expect(page).to have_content("RSpecを学ぶ")
+    expect(page).to have_content("システムスペックについて理解を深める")
+  end
 end
