176021ce11626a3dc6ddb4da910435d6752089d5
kimu805 <kimukimuok805@gmail.com>
2025-04-11 16:59:08 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get "todos/index"
-  get "todos/new"
-  get "todos/create"
-  get "todos/show"
-  get "todos/edit"
-  get "todos/update"
-  get "todos/destroy"
+  resources :todos
   resources :articles
 end
