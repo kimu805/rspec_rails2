2a02fec8240321d5fa8c40ccca2e4271f1ba592a
kimu805 <kimukimuok805@gmail.com>
2025-04-11 16:51:30 +0900

    rails g controller todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "todos/index"
+  get "todos/new"
+  get "todos/create"
+  get "todos/show"
+  get "todos/edit"
+  get "todos/update"
+  get "todos/destroy"
   resources :articles
 end
---
+++ spec/requests/todos_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe "Todos", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/todos/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/todos/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/todos/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/todos/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/todos/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/todos/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/todos/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
