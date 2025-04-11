27703b8d6e275ff4bb81444360d0392d8336e185
kimu805 <kimukimuok805@gmail.com>
2025-04-11 17:41:47 +0900

    Update todos_spec.rb

--- spec/system/todos_spec.rb
+++ spec/system/todos_spec.rb
@@ -5,26 +5,38 @@ RSpec.describe "Todo管理", type: :system do
     driven_by(:rack_test)
   end
 
-  it "Todo一覧を表示する" do
-    Todo.create!(title: "testTodo", description: "これはテストTodoの説明です。")
-    visit todos_path
-    expect(page).to have_content("Todo一覧")
-    expect(page).to have_content("testTodo")
-    expect(page).to have_content("これはテストTodoの説明です。")
-    expect(page).to have_content("未完了")
+  context "正常系" do
+    it "Todo一覧を表示する" do
+      Todo.create!(title: "testTodo", description: "これはテストTodoの説明です。")
+      visit todos_path
+      expect(page).to have_content("Todo一覧")
+      expect(page).to have_content("testTodo")
+      expect(page).to have_content("これはテストTodoの説明です。")
+      expect(page).to have_content("未完了")
+    end
+
+    it "新規Todoを作成できる" do
+      visit new_todo_path
+
+      fill_in "タイトル", with: "RSpecを学ぶ"
+      fill_in "説明", with: "システムスペックについて理解を深める"
+
+      click_button "作成"
+
+      expect(current_path).to eq(todos_path)
+
+      expect(page).to have_content("RSpecを学ぶ")
+      expect(page).to have_content("システムスペックについて理解を深める")
+    end
   end
 
-  it "新規Todoを作成できる" do
-    visit new_todo_path
+  context "バリデーション" do
+    it "タイトルと説明が未入力の場合、エラーメッセージが表示される" do
+      visit new_todo_path
 
-    fill_in "タイトル", with: "RSpecを学ぶ"
-    fill_in "説明", with: "システムスペックについて理解を深める"
-
-    click_button "作成"
-
-    expect(current_path).to eq(todos_path)
-
-    expect(page).to have_content("RSpecを学ぶ")
-    expect(page).to have_content("システムスペックについて理解を深める")
+      click_button "作成"
+      expect(page).to have_content("Title can't be blank")
+      expect(page).to have_content("Description can't be blank")
+    end
   end
 end
